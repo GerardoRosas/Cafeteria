712882b8c6b9ecff1cb3d3b71702c0cdc9bc44f1
GerardoRosas <rosas.ruizlg@gmail.com>
2022-03-22 12:34:56 -0600

    Header finalizado

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+
 function cssCompile(done) {
     //Compilar sass 
     //pasos: 1 -identidicar archivos, 2 - compilar, 3 - guardar .css
@@ -17,12 +20,24 @@ function cssCompile(done) {
 
 function watchChanges() {
     watch('./src/scss/**/*.scss', cssCompile);
+    watch('./src/img/**/*', images);
 }
 
-function tareaDefault() {
-    console.log('Soy la tarea por default')
+function images(done) {
+    src('./src/img/**/*')
+        .pipe(imagemin({optimizationLevel: 3}))
+        .pipe(dest('build/img'));
+    done()
+}
+
+function versionWebp() {
+    return src('src/img/**/*.{png,jpg}')
+        .pipe(webp())
+        .pipe(dest('build/img'));
 }
 
 exports.cssCompile = cssCompile;
 exports.watchChanges = watchChanges;
-exports.default = series( cssCompile, watchChanges );
+exports.images = images;
+exports.versionWebp = versionWebp;
+exports.default = series( images, versionWebp, cssCompile, watchChanges );
